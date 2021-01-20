677c2df08de27184e38e14c3b822f8226f1ca820
Yu Yang Quek <yquek@lely.com>
2021-01-20 20:24:30 +0100

    Accept path to parameters json file instead of using hardcoded path

--- src/stitch_video.cpp
+++ src/stitch_video.cpp
@@ -9,14 +9,13 @@ int main(int argc, char** argv)
 {
   if ( argc < 4 )
   {
-    std::cout << "usage: ./sync_videos <video group path> <video ID 1> <video ID 2> ..." << std::endl;
+    std::cout << "usage: ./sync_videos <video group path> <parameters json path> <video ID 1> <video ID 2> ..." << std::endl;
     return -1;
   }
 
   VideoSynchronizer vid_sync(argv[1]);
-  std::vector<std::string> IDs(argv+2,argv+argc);
-
-  std::string jpath("/home/quekyuyang/Projects/stitch/data/231120-2017/cpp.json");
+  std::string jpath(argv[2]);
+  std::vector<std::string> IDs(argv+3,argv+argc);
 
   auto frameset = vid_sync.getFrames();
   std::map<std::string,cv::Mat> imgs;
