5a5407b0d895e44b060738a749f1e7d3b5bf24cb
Yu Yang Quek <yquek@lely.com>
2021-02-06 10:41:30 +0100

    Refactorized repeated code

--- src/image_stitch/image_struct.cpp
+++ src/image_stitch/image_struct.cpp
@@ -35,55 +35,40 @@ void Image::getKpsAndDes(std::vector<cv::KeyPoint> &kps_filter,cv::Mat &des_filt
                          std::vector<cv::Rect> ROIs_features,
                          const bool histequal)
 {
+  std::vector<cv::KeyPoint> *kps=nullptr;
+  cv::Mat des;
   if (histequal)
   {
-    if (_kps_histequal.empty())
-      return;
-
-    std::vector<unsigned int> des_row_mask;
-    auto n_kps = _kps_histequal.size();
-    for (int i = 0; i < n_kps; i++)
-    {
-      bool is_in_BB = false;
-      for (auto ROI_features : ROIs_features)
-        is_in_BB = is_in_BB || ROI_features.contains(_kps_histequal[i].pt);
-
-      if (is_in_BB)
-      {
-        kps_filter.push_back(_kps_histequal[i]);
-        des_row_mask.push_back(i);
-      }
-    }
-
-    des_filter.create(des_row_mask.size(),_des_histequal.cols,_des_histequal.type());
-    auto n_des = des_row_mask.size();
-    for (int i = 0; i < n_des; i++)
-      _des_histequal.row(des_row_mask[i]).copyTo(des_filter.row(i));
+    kps = &_kps_histequal;
+    des = _des_histequal;
   }
   else
   {
-    if (_kps.empty())
-      return;
-    std::vector<unsigned int> des_row_mask;
-    auto n_kps = _kps.size();
-    for (int i = 0; i < n_kps; i++)
-    {
-      bool is_in_BB = false;
-      for (auto ROI_features : ROIs_features)
-        is_in_BB = is_in_BB || ROI_features.contains(_kps[i].pt);
-
-      if (is_in_BB)
-      {
-        kps_filter.push_back(_kps[i]);
-        des_row_mask.push_back(i);
-      }
-    }
-
-    des_filter.create(des_row_mask.size(),_des.cols,_des.type());
-    auto n_des = des_row_mask.size();
-    for (int i = 0; i < n_des; i++)
-      _des.row(des_row_mask[i]).copyTo(des_filter.row(i));
+    kps = &_kps;
+    des = _des;
   }
+  if (kps->empty())
+    return;
+
+  std::vector<unsigned int> des_row_mask;
+  auto n_kps = kps->size();
+  for (int i = 0; i < n_kps; i++)
+  {
+    bool is_in_BB = false;
+    for (auto ROI_features : ROIs_features)
+      is_in_BB = is_in_BB || ROI_features.contains((*kps)[i].pt);
+
+    if (is_in_BB)
+    {
+      kps_filter.push_back((*kps)[i]);
+      des_row_mask.push_back(i);
+    }
+  }
+
+  des_filter.create(des_row_mask.size(),des.cols,des.type());
+  auto n_des = des_row_mask.size();
+  for (int i = 0; i < n_des; i++)
+    des.row(des_row_mask[i]).copyTo(des_filter.row(i));
 }
 
 std::string Image::getID()
